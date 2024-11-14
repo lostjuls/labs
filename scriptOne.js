4615148a131d5439239ebde6b5916fb954366605
lostjuls <julescedeno05@gmail.com>
2024-11-13 22:38:02 -0500

    lab 8

--- scriptOne.js
+++ scriptOne.js
@@ -1,12 +1,113 @@
-var _age = 2000;
+var FirstName = "Jules";
+console.log(FirstName);
+var age = 18;
+console.log(age);
 
-var age;
-age = 1000
+// String
+let name = "Sarai";
+console.log("Name:", name);
 
-console. log(age)
+// Number
+let grade_level = 2;
+console.log("grade_level:", grade_level);
 
-let userName = "bob";
+// Boolean
+let isStudent = true;
+console.log("Is Student:", isStudent);
 
-let userName = "jeff"
+// Undefined
+let address;
+console.log("Address:", address);
+
+// RollNumber variable
+let RollNumber = 9;
+
+// Greater than 10?
+if (RollNumber > 10) {
+  console.log("YES, it's greater than 10");
+}
+// Not greater than 10?
+if (RollNumber < 10) {
+    console.log("Nah, it's doing it's own thing");
+  }
+// Boolean
+let isMember = true;
+
+// isMember is true?
+if (isMember) {
+  console.log("Member discount applied.");
+}
+
+let startValue = 50;
+
+if (startValue > 0) 
+    {
+  console.log("It's positive.");} 
+  else if (startValue < 0) 
+    {
+console.log("It's negative.");} 
+  else {
+  console.log("It's  zero.");
+}
+
+let a = 10;
+let b = 20;
+
+console.log("a == b:", a == b);
+console.log("a === b:", a === b);
+console.log("a != b:", a != b);
+console.log("a > b:", a > b);
+console.log("a < b:", a < b);
+console.log("a >= b:", a >= b);
+console.log("a <= b:", a <= b);
+
+let firstName_1 = "Vanessa";
+let lastName_1 = "Valdes";
+let age_1 = 18;
+
+console.log("Hello, " + firstName_1 + " " + lastName_1 + ", you are " + age_1 + " years old.");
+
+let num = 50;
+
+if (num > 0 & num < 100) {
+  console.log("The number is within range :D");
+}
+
+let hasCar = true;
+let hasLicense = true;
+let hasInsurance = true;
+
+if (hasCar & hasLicense & hasInsurance) {
+  console.log("You can drive legally.");
+} else {
+  console.log("Check your driving eligibility.");
+}
+
+let num_1 = 21;
+
+if (num_1 % 2 === 0) {
+  console.log("The number is even.");
+} else {
+  console.log("The number is odd.");
+
+  
+}
+
+let score = 85;
+
+if (score >= 90) {
+  console.log("Grade A.");
+} else if (score >= 80 & score <= 89) {
+  console.log("Grade B.");
+} else {
+  console.log("Grade C.");
+}
+
+
+
+
+
+
+
 
 
